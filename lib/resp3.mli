29992bde1cd9651cf95b3e784f91b3b2637ade2b
Weihang Lo <me@weihanglo.tw>
2021-02-25 10:06:26 +0800

    feat: interface

--- lib/resp3.mli
+++ lib/resp3.mli
@@ -1,1 +1,21 @@
-val hello : unit -> unit
+type value =
+  | Blob of bytes
+  | String of string
+  | Error of string
+  | Number of int64
+  | Null
+  | Double of float
+  | Boolean of bool
+  | BlobError of { code : string; message : string; }
+  | VerbatimString of { format : string; text: string; }
+  | BigNumber of Num.num
+  | Array of value list
+  | Map of (value, value) Hashtbl.t
+  | Set of (value, unit) Hashtbl.t
+  | Attribute of (value, value) Hashtbl.t
+  | Push of { kind : string; messages : value list }
+(* TODO: handle stream aggregated data types *)
+
+val parse : string -> value
+
+val to_string : value -> string
